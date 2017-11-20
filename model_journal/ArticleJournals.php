7254cfb7380eecd8b2fe771e580c2f0ab5a253a5
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-20 20:46:03 +0700

    modify component system location nb. module core path condition

--- model_journal/ArticleJournals.php
+++ model_journal/ArticleJournals.php
@@ -306,7 +306,7 @@ class ArticleJournals extends CActiveRecord
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
+				'filter' => Yii::app()->controller->widget('application.libraries.core.components.system.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'creation_date',
 					'language' => 'en',
@@ -338,7 +338,7 @@ class ArticleJournals extends CActiveRecord
 				'htmlOptions' => array(
 					'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
+				'filter' => Yii::app()->controller->widget('application.libraries.core.components.system.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'modified_date',
 					'language' => 'en',
