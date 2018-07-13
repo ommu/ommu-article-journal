0be2d24a3225ba4084cf635ab97eeb7e0d1d5d9e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-13 12:53:06 +0700

    move getGridColumn function to trait

--- controllers/journal/FileController.php
+++ controllers/journal/FileController.php
@@ -179,15 +179,7 @@ class FileController extends Controller
 			$model->attributes=$_GET['ArticleJournalFile'];
 		}
 
-		$columnTemp = array();
-		if(isset($_GET['GridColumn'])) {
-			foreach($_GET['GridColumn'] as $key => $val) {
-				if($_GET['GridColumn'][$key] == 1) {
-					$columnTemp[] = $key;
-				}
-			}
-		}
-		$columns = $model->getGridColumn($columnTemp);
+		$columns = $model->getGridColumn($this->gridColumnTemp());
 
 		$this->pageTitle = Yii::t('phrase', 'Article Journal Files');
 		$this->pageDescription = '';
